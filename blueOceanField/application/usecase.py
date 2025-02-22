c8c514b2d1f5d18dde2e24243953b1ce920eef50
nmskm240 <utc.am240@gmail.com>
2025-02-22 12:36:51 +0000

    refact: usecaseを関数モジュール化

--- blueOceanField/application/usecase.py
+++ blueOceanField/application/usecase.py
@@ -1,20 +1,15 @@
 import ccxt
 from blueOceanField.domain.feature import FeatureProcessMeta
-from blueOceanField.domain.market import ExchangePlace, IExchange, IOhlcvRepository, Symbol
+from blueOceanField.domain.market import *
 
 
-class GetFeatureProcessMetadataUsecase:
-    def execute(self) -> list[FeatureProcessMeta]:
-        return list(FeatureProcessMeta.registry.values())
+def get_feature_process_metadata() -> list[FeatureProcessMeta]:
+    return list(FeatureProcessMeta.registry.values())
 
+def get_supported_exchanges() -> list[ExchangePlace]:
+    exchanges = [ExchangePlace(e) for e in ccxt.exchanges]
+    exchanges.append(ExchangePlace("backtest"))
+    return exchanges
 
-class GetSupportedExchangesUsecase:
-    def execute(self) -> list[ExchangePlace]:
-        exchanges = [ExchangePlace(e) for e in ccxt.exchanges]
-        exchanges.append(ExchangePlace("backtest"))
-        return exchanges
-
-
-class GetSupportedSymbolsUsecase:
-    async def execute_async(self, exchange: IExchange) -> list[Symbol]:
-        return await exchange.get_all_symbols_async()
+async def get_supported_symbols_async(exchange: IExchange) -> list[Symbol]:
+    return await exchange.get_all_symbols_async()
--- blueOceanField/presentation/grpc.py
+++ blueOceanField/presentation/grpc.py
@@ -1,7 +1,7 @@
 from injector import Module, inject, singleton
 from blueOceanField.application.converter import GrpcConverter
 import blueOceanField.application.generated as proto
-import blueOceanField.application.usecase as uc
+import blueOceanField.application.usecase as usecase
 from blueOceanField.domain.market import IExchange, IOhlcvRepository
 
 
@@ -17,15 +17,13 @@ class GrpcModule(Module):
 
 class FeatureProcessHandler(proto.FeatureProcessServiceServicer):
     def GetFeatureProcessMeta(self, request, context):
-        usecase = uc.GetFeatureProcessMetadataUsecase()
-        processes = [GrpcConverter.to_grpc(process) for process in usecase.execute()]
+        processes = [GrpcConverter.to_grpc(process) for process in usecase.get_feature_process_metadata()]
         return proto.FeatureProcessMetaResponse(metadata=processes)
 
 
 class MarketHandler(proto.MarketServiceServicer):
     def GetExchanges(self, request: proto.GetExchangesRequest, context):
-        usecase = uc.GetSupportedExchangesUsecase()
-        places = [GrpcConverter.to_grpc(place) for place in usecase.execute()]
+        places = [GrpcConverter.to_grpc(place) for place in usecase.get_supported_exchanges()]
         return proto.GetExchangesResponse(exchangePlaces=places)
 
     async def GetSymbols(self, request: proto.GetSymbolsRequest, context):
@@ -33,9 +31,8 @@ class MarketHandler(proto.MarketServiceServicer):
 
         target_exchange = GrpcConverter.from_grpc(request.exchange)
         exchange_context = AppContext.get_or_create_exchange_context(target_exchange)
-        usecase = uc.GetSupportedSymbolsUsecase()
         symbols = [
             GrpcConverter.to_grpc(symbol)
-            for symbol in await usecase.execute_async(exchange_context.get(IExchange))
+            for symbol in await usecase.get_supported_symbols_async(exchange_context.get(IExchange))
         ]
         return proto.GetSymbolsResponse(symbols=symbols)
